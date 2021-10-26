84e4d120e8f12bf2c391079f364e857294725eba
Lucas Ramos <87498874+devLucasRamos@users.noreply.github.com>
2021-10-26 12:34:43 -0300

    Project Start

---
+++ Entity/Serie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebSeries.Enum;
+
+namespace WebSeries.Entity
+{
+    public class Serie : EntityBase
+    {
+        public Genero Genero { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public int Ano { get; set; }
+    }
+}
